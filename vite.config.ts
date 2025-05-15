cd8334bdbd6ac5aaf1554756dd1c213cd58b08d9
ch4ngo <marco.dacio@icloud.com>
2025-05-15 11:21:22 +0200

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/tradicoes-harburg-folclore-web/' : '/',
+  base: mode === 'production' ? '/rancho/' : '/',
   server: { host: '::', port: 8080 },
   plugins: [react(), tsconfigPaths()],
 }))
